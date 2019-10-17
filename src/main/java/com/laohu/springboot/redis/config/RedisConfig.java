3a5eb4688acfbc3ecab1f97768f8b9027f5a89e1
Holland <chdtunnelworker@163.com>
2019-10-17 18:13:27 +0800

    1.第七章redis

---
+++ src/main/java/com/laohu/springboot/redis/config/RedisConfig.java
@@ -0,0 +1,61 @@
+package com.laohu.springboot.redis.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+/**
+ * @program: springboot
+ * @description: redis配置类
+ * @author: Holland
+ * @create: 2019-10-17 10:42
+ **/
+@Configuration
+public class RedisConfig {
+
+    private RedisConnectionFactory connectionFactory = null;
+
+    @Bean(name = "RedisConnectionFactory")
+    public RedisConnectionFactory initRedisConnectionFactory(){
+        if(this.connectionFactory != null){
+            return this.connectionFactory;
+        }
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        //配置最大连接空闲数
+        poolConfig.setMaxIdle(30);
+        //设置最大连接数
+        poolConfig.setMaxTotal(50);
+        //使劲儿中最大等待毫秒数
+        poolConfig.setMaxWaitMillis(2000);
+        //创建jedis连接工厂,将连接池配置注入
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory(poolConfig);
+        connectionFactory.setHostName("192.168.206.128");
+        connectionFactory.setPort(6379);
+        connectionFactory.setPassword("123456");
+        this.connectionFactory = connectionFactory;
+        //获取单机的redis配置
+        RedisStandaloneConfiguration rsCfg = connectionFactory.getStandaloneConfiguration();
+        return connectionFactory;
+    }
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<Object,Object> initRedisTemplate(){
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //redisTemplate会自动初始化StringRedisSerializer,可以通过下面方法直接获取
+        RedisSerializer<String> stringSerializer = redisTemplate.getStringSerializer();
+        //将键和hash的序列化器全部设置为字符串序列化器
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setValueSerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
+        redisTemplate.setConnectionFactory(initRedisConnectionFactory());
+        return redisTemplate;
+    }
+}
--- src/test/java/com/laohu/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/laohu/springboot/SpringbootApplicationTests.java
@@ -4,9 +4,15 @@ import com.laohu.springboot.AOP.appointProgramming.HelloService;
 import com.laohu.springboot.AOP.appointProgramming.HelloServiceImpl;
 import com.laohu.springboot.AOP.appointProgramming.MyInterceptor;
 import com.laohu.springboot.AOP.appointProgramming.ProxyBean;
+import com.laohu.springboot.redis.config.RedisConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -15,5 +21,18 @@ public class SpringbootApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    /**
+     * 测试通过redisTemplate向redis中保存数据
+     * 如果不指定序列化器,会默认采用JdkSerializationRedisSerializer序列化器,会使我们保存的数据以16进制显示,看不到具体字符串
+     * 因此:需要指定字符串序列化器
+     */
+    @Test
+    public void testRedisTemplate(){
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(RedisConfig.class);
+        RedisTemplate redisTemplate = context.getBean(RedisTemplate.class);
+        redisTemplate.opsForValue().set("key1","value1");
+        redisTemplate.opsForHash().put("hash","field","hvalue");
     }
 }
